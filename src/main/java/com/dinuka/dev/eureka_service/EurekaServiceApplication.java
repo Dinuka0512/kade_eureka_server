7eb3bcc38e1eaa0d58ab51a0610e247292149f61
Dinuka Lakmal <177092200+Dinuka0512@users.noreply.github.com>
2026-08-26 14:17:37 +0530

    testinng......

--- src/main/java/com/dinuka/dev/eureka_service/EurekaServiceApplication.java
+++ src/main/java/com/dinuka/dev/eureka_service/EurekaServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServiceApplication {
-//    TESTING CI/CD
+//    TESTING CI/CD...
     public static void main(String[] args) {
         SpringApplication.run(EurekaServiceApplication.class, args);
     }
